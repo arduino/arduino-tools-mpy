d407981a6a9ab6fa63a6650cdd840887bdc860a7
ubi de feo <me@ubidefeo.com>
2024-03-06 19:59:52 +0100

    reworked help method show_commands()
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- arduino_utils/arduino_utils.py
+++ arduino_utils/arduino_utils.py
@@ -437,6 +437,10 @@ def clear_terminal(cursor_visible = True):
 
 def show_commands():
   clear_terminal()
+  with open(f'{get_module_path()}/amp_help.txt', 'r') as help_file:
+    for line in help_file.readlines():
+      print(line, end = '')
+    help_file.close()
 
 
 # TODO:
