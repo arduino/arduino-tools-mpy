69a843c2cda54a67456c57b4dcc16c2504f05f6c
ubi de feo <me@ubidefeo.com>
2024-03-09 20:19:58 +0100

    Cleaned imports to speed up bootloader process: 2s gained
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- arduino_tools/__init__.py
+++ arduino_tools/__init__.py
@@ -3,7 +3,7 @@ __license__ = "MIT License"
 __version__ = "0.1.0"
 __maintainer__ = "ubi de feo [github.com/ubidefeo]"
 
-from .amp_common import *
-from .arduino_tools import *
-from .amp_network import *
-from .amp_settings import *
+# from .amp_common import *
+# from .arduino_tools import *
+# from .amp_network import *
+# from .amp_settings import *
